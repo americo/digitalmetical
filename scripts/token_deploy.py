bf92fb53c2b752ea2a4e1fa35e0d7352ceee62e6
Américo Júnior <62674418+americo@users.noreply.github.com>
2022-05-15 23:24:47 +0200

    Update token_deploy.py

--- scripts/token_deploy.py
+++ scripts/token_deploy.py
@@ -5,9 +5,9 @@ from web3 import Web3
 
 from scripts.helpful_scripts import get_account
 
-initial_supply = Web3.toWei(10000, "ether")
+initial_supply = Web3.toWei(1000, "ether")
 
 
 def main():
     account = get_account()
-    return Token.deploy("Evil Coin", "EVIL", 18, initial_supply, {"from": account})
+    return Token.deploy("Digital Metical", "EVIL", 18, initial_supply, {"from": account})
